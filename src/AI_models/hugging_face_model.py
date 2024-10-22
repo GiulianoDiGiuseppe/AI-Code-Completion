3c7f7fa04e2a692d25e73c50114116b8f6b61a80
Giuliano Di Giuseppe <digigiu00@gmail.com>
2024-10-23 00:20:54 +0200

    fix : extract only new code

--- src/AI_models/hugging_face_model.py
+++ src/AI_models/hugging_face_model.py
@@ -29,7 +29,7 @@ class ModelHandler:
         tokenizer = AutoTokenizer.from_pretrained(checkpoint)
         return tokenizer
 
-    def generate(self, input_text,skip_special_tokens : bool =False):
+    def generate(self, input_text, skip_special_tokens: bool = False):
         # Encode the input text
         input_ids = self.tokenizer.encode(input_text, return_tensors='pt').to(self.device)
         
@@ -39,8 +39,19 @@ class ModelHandler:
             attention_mask[input_ids == self.param_dict['pad_token_id']] = 0  # Set padding indices to 0 in the mask
 
         # Generate outputs using the model with the specified parameters
-        outputs = self.model.generate(input_ids,pad_token_id=self.tokenizer.eos_token_id, attention_mask=attention_mask, **self.param_dict)
-        
-        # Decode the generated texts into a list
-        generated_texts = [self.tokenizer.decode(output, skip_special_tokens=skip_special_tokens) for output in outputs]
+        outputs = self.model.generate(input_ids, pad_token_id=self.tokenizer.eos_token_id, attention_mask=attention_mask, **self.param_dict)
+
+        generated_texts = []
+        for output in outputs:
+            # Find where the input ends
+            input_len = input_ids.shape[-1]  # Get the length of the input (number of tokens in input)
+
+            # Extract the newly generated tokens (after the input)
+            new_tokens = output[input_len:].tolist()  # Convert the tensor to a list of token IDs
+            
+            # Decode only the new tokens
+            generated_text = self.tokenizer.decode(new_tokens, skip_special_tokens=skip_special_tokens)
+            generated_texts.append(generated_text)
+
+        # Return the newly generated text
         return generated_texts
